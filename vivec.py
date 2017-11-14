f541535463fb21ef688f0b3d40ff32cc62175406
Unknown <steinar.a.volden@gmail.com>
2017-11-14 14:54:49 +0500

    Ничего не работает!
    Но так и должно быть

--- vivec.py
+++ vivec.py
@@ -8,9 +8,34 @@ cash = 100
 def wisdom(strategies, needs, prices, profits):
     d_avg = 0
     j_avg = 0
+    needlist = []
+    P_M = {}
+
+    for i in range(1, 10):
+        for j in range(1, 10):
+            needlist.append([i, j])
+
+    # Вот здесь надо адаптировать формулу под
+    # то, что у меня вместо чисел в стратегиях
+    # листы.
+
+    for i, _a in enumerate(strategies):
+        P_M[i] = {}
+        for j, _p in enumerate(needlist):
+            if _a == _p:
+                P_M[i][j] = _a * prices[1]
+            elif _a > _p:
+                P_M[i][j] = _p * prices[1] + (_a - _p) * prices[0]
+            else:
+                P_M[i][j] = _a * prices[1] + (_p - _a) * prices[2]
+
+    for i in P_M.keys():
+        print(P_M[i])
+
     for client in needs:
         d_avg += client[0]
         j_avg += client[1]
+
     d_avg = d_avg / 5
     j_avg = j_avg / 5
     print(d_avg, j_avg)
@@ -29,8 +54,6 @@ def move():
 
     if cash <= 4:
         return 'You lose'
-    elif cash >= 200:
-        return 'You win'
 
     prices = [15, 4]
 
@@ -44,13 +67,8 @@ def move():
             strategies.append([i, j])
 
     for i in range(5):
-        kek = round(random.uniform(0.0, 0.5), 1)
-        if random.randint(0, 10) % 2 == 0:
-            d_need = 0.5 + kek
-            j_need = 0.5 - kek
-        else:
-            j_need = 0.5 + kek
-            d_need = 0.5 - kek
+        d_need = round(random.uniform(0.0, 0.5), 1)
+        j_need = round(random.uniform(0.0, 0.5), 1)
         needs.append([d_need, j_need])
 
     chosen = wisdom(strategies, needs, prices, profits)
