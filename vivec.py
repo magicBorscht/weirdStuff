ecb39ae578f83135aa07033906cc4b953a916450
Unknown <steinar.a.volden@gmail.com>
2017-11-28 15:05:57 +0500

    Я создал ПРОИГРЫВАТЕЛЬ
    Потому что он постоянно ПРОИГРЫВАЕТ, АААААААААХАХАХАХАХАХАХАХАХАХАХАХХАХАХАХА

--- vivec.py
+++ vivec.py
@@ -1,8 +1,10 @@
 import random
+import numpy
 
 d_count = 0
 j_count = 0
 cash = 100
+old_cash = 100
 # Каким-то образом надо учесть в формулах ниже кол-во барахла на складе.
 # Покупка действительно будет дешевле при наличии уже имеющихся предметов.
 # Я пошёл пить кофе.
@@ -56,7 +58,7 @@ def wisdom(strategies, needs, prices, profits):
 
             elif _a[0] + d_count > _p[0] and _a[1] + j_count == _p[1]:
                 P_M[i][j] = moar(_a[0], _p[0], d_prft, d_count) + eqq(_a[1], j_prft, j_count)
-                # p * prices[1] + (_a - _p) * prices[0]
+
             elif _a[0] + d_count == _p[0] and _a[1] + j_count > _p[1]:
                 P_M[i][j] = eqq(_a[0], d_prft, d_count) + moar(_a[1], _p[1], j_prft, j_count)
 
@@ -77,8 +79,9 @@ def wisdom(strategies, needs, prices, profits):
 
             elif _a[0] + d_count < _p[0] and _a[1] + j_count > _p[1]:
                 P_M[i][j] = less(_a[0], _p[0], d_prft, d_count) + moar(_a[1], _p[1], j_prft, j_count)
+
             else:
-                print("that should not happen")  # P_M[i][j] = _a * prices[1] + (_p - _a) * prices[2]
+                print("that should not happen")
                 print(_a, _p)
 
     # And now for something completely different. The possibilities
@@ -109,83 +112,22 @@ def wisdom(strategies, needs, prices, profits):
     j_probs.append(sum(j_needs) / 6)
     # print("j_probs =", j_probs)
     the_str.append(j_probs.index(max(j_probs)))
-    print(the_str)
+    # print(the_str)
 
     for i in range(len(needlist)):
         prob.append((d_probs[needlist[i][0]] + j_probs[needlist[i][1]]) / 2)
 
-    print(needlist[prob.index(max(prob))])
+    # print(needlist[prob.index(max(prob))])
     b_i = []
     a_i = []
     for line in P_M.values():
         buf = list(line.values())
-        # print(buf)
-        a_i.append(min(buf))
         _b_i = 0
         for i, b in enumerate(buf):
             _b_i += b * prob[i]
-        print(_b_i)
         b_i.append(_b_i)
-    K_i = max(a_i)
-    answer = a_i.index(K_i)
-    print('пыщь' * 20)
-    print(K_i, answer + 1)
-    print("blbl" * 20)
-    print(" ")
-
-    b_i_j = [list(element.values()) for element in P_M.values()]
-
-    _tmp = list(zip(*b_i_j))
-
-    for column in _tmp:
-        beta.append(max(column))
-
-    r_i_j = []
-
-    b_i_j_t = list(zip(*b_i_j))
-
-    for i, row in enumerate(b_i_j_t):
-        r_i_j.append([])
-        for b in row:
-            r_i_j[i].append(beta[i] - b)
-
-    r_i_j = list(zip(*r_i_j))
-
-    r_i = [max(row) for row in r_i_j]
-
-    rres = min(r_i)
-    ares = r_i.index(rres) + 1
-    cntres = r_i.count(rres)
-
-    if cntres > 1:
-        print("минимальный риск составляет {} в {} стратегиях, например, {}".format(rres, cntres, ares))
-    else:
-        print("минимальный риск составляет {} в стратегии №{}".format(rres, ares))
-
-    beta = []
-    d = 0.2
-
-    _tmp = list(zip(*b_i_j))
-
-    k_III = []
-
-    left = []
-
-    for column in _tmp:
-        k_III.append(d * min(column) + (1 - d) * max(column))
-
-    K_III = max(k_III)
-
-    aresIII = k_III.index(K_III) + 1
-    cntresIII = k_III.count(K_III)
-
-    '''if cntresIII > 1:
-        print("K III составляет {} в {} стратегиях, например, {} при коэффициенте {}".format(K_III, cntresIII, aresIII,
-                                                                                             d))
-    else:
-        print("K III составляет {} в стратегии №{} при коэффициенте {}".format(K_III, aresIII, d))
-
-    # А теперь для каждой стратегии надо помножить вероятность на возможный выигрыш'''
+    ge = 12  # P_M[the_str[0] * the_str[1] - 1][the_str[0] * the_str[1] - 1]
+    print("ВНИМАНИЕ! ГЕ: ", ge)
 
     # Это не нужно и будет удалено (возможно)
     ''' for client in needs:
@@ -202,21 +144,67 @@ def wisdom(strategies, needs, prices, profits):
         varg.append(kek)
     print('varg = ', varg)
     return varg.index(max(varg)) '''
-    return 12
+    return the_str
 
+def zakup(strtg):
+    global d_count
+    global j_count
+    global cash
+    ge = d_count
+    gege = j_count
+    sauce = [0, 0]
+
+    for i in range(ge, strtg[0]):
+        if cash < 15:
+            print("Сперва прикупи себе денег")
+            break
+        d_count += 1
+        cash -= 15
+        sauce[0] += 1
+
+    for i in range(gege, strtg[1]):
+        if cash < 4:
+            print("Даже джек купить не можешь")
+            break
+        j_count += 1
+        cash -= 4
+        sauce[1] += 1
+    print("Приобретено {} дисплеев и {} джеков, теперь их {} и {}".format(sauce[0], sauce[1], d_count, j_count))
+
+
+def repair(customers):
+    global cash
+    global j_count
+    global d_count
+    for person in customers:
+        if person[0] == 1 and d_count > 0:
+            d_count -= 1
+            cash += 22
+            print("Починен дисплей, получено 22 чего бы то ни было")
+        elif person[0] == 1 and d_count == 0:
+            cash -= 10
+            print("Не починен дисплей, репутация ПАДАЕТ!")
+
+        if person[1] == 1 and j_count > 0:
+            j_count -= 1
+            cash += 6
+            print("Починен джек, получено 6 чего бы то ни было")
+        elif person[1] == 1 and j_count == 0:
+            cash -= 5
+            print("Не починен джек, репутация ПАДАЕТ!")
+
+    if cash < 0:
+        cash = 0
 
 def move():
     global cash
     global d_count
     global j_count
+    global old_cash
 
-    if cash <= 4:
-        return 'You lose'
-
+    customers = []
     prices = [15, 4]
-
     profits = [22, 6]
-
     strategies = []
     needs = []
 
@@ -230,8 +218,21 @@ def move():
         needs.append([d_need, j_need])
 
     chosen = wisdom(strategies, needs, prices, profits)
-    print(chosen)
-    print("this is NEEDS:", needs)
+    # print(chosen)
+    # print("these are NEEDS:", needs)
+    zakup(chosen)
+    for k in needs:
+        customers.append([numpy.random.choice([0, 1], p=[k[0], 1 - k[0]]),
+                          numpy.random.choice([0, 1], p=[k[1], 1 - k[1]])])
+    # print(customers)
+    repair(customers)
+    print("Денег в кассе: {}, выручка за день: {}".format(cash, cash - old_cash))
+    print("К концу дня на складе {} дисплеев и {} джеков".format(d_count, j_count))
+    print('\n', '=' * 40, '\n')
+    old_cash = cash
 
-
-move()
+for i in range(100):
+    if cash < 4 and j_count == 0 and d_count == 0:
+        print("Проиграл за {} дней".format(i))
+        break
+    move()
