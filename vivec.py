bfb4714b033f693bc98b292a9543b7599f60f1ba
Unknown <steinar.a.volden@gmail.com>
2017-11-30 00:33:32 +0500

    KIROV REPORTING
    Теперь-то он точно готов

--- vivec.py
+++ vivec.py
@@ -5,20 +5,26 @@ d_count = 0
 j_count = 0
 cash = 1000
 old_cash = 1000
-d_gl_profits = (220, -50, -60, 220)
-j_gl_profits = (40, -42, -50, 60)
-d_sale_price = 250
+d_gl_profits = (225, -50, -47, 250) # Цена за единицу товара, цена хранения, репутационные убытки,
+j_gl_profits = (54, -42, -25, 60)   # чистая прибыль за ремонт
+d_sale_price = 250                  # Другая чистая прибыль за ремонт (зачем я их разделил?)
 j_sale_price = 60
+time = 0
+lose = False
 
-switcher = 1
-raz = 3
-dva = 3
+counters = [0, 0, 0, 0, 0, 0, 0]
+# Починено мониторов, джеков, не починено мониторов, джеков, куплено мониторов, джеков, покупатели
 
-i_am_lazy = 1000
+switcher = 0    # Переключение в режим тупицы, в качестве стратегии берутся переменные raz - кол-во дисплеев
+raz = 3         # и dva - кол-во джеков
+dva = 4
+
+i_am_lazy = 1000    # Количество ходов (мне было лень листать в конец файла)
 
 # Каким-то образом надо учесть в формулах ниже кол-во барахла на складе.
 # Покупка действительно будет дешевле при наличии уже имеющихся предметов.
 # Я пошёл пить кофе.
+# Комментарии оставлены для истории
 
 
 def eqq(a, profit, c):
@@ -34,6 +40,7 @@ def less(a, b, profit, c):
 
 
 def wisdom(strategies, needs):
+    """Здесь находится РАЗУМ РОБОТА!"""
     global d_count
     global j_count
     needlist = []
@@ -42,8 +49,7 @@ def wisdom(strategies, needs):
     global j_gl_profits
     global d_sale_price
     global j_sale_price
-    # d_prft = [70, -40, -67, 220]
-    # j_prft = [20, -25, -45, 60]
+
     d_prft = list(d_gl_profits)
     j_prft = list(j_gl_profits)
     d_prft[0] = d_sale_price - d_prft[0]
@@ -134,13 +140,103 @@ def wisdom(strategies, needs):
     j_probs.append(sum(j_needs) / 6)
     # print("j_probs =", j_probs)
     the_str.append(j_probs.index(max(j_probs)))
-
-
     # print(the_str)
 
+    # Я устал, это брутфорс
+    d_yep = []
+    j_yep = []
+    d_nope = []
+    j_nope = []
+
+    for need in needs:
+        d_yep.append(need[0])
+        j_yep.append(need[1])
+        d_nope.append(1 - need[0])
+        j_nope.append(1 - need[1])
+
+    d_probs = []
+    j_probs = []
+    yep = d_yep
+    nope = d_nope
+
+    d_probs.append(nope[0] * nope[1] * nope[2] * nope[3] * nope[4])
+    d_probs.append((yep[0] * nope[1] * nope[2] * nope[3] * nope[4]) +
+                   (nope[0] * yep[1] * nope[2] * nope[3] * nope[4]) +
+                   (nope[0] * nope[1] * yep[2] * nope[3] * nope[4]) +
+                   (nope[0] * nope[1] * nope[2] * yep[3] * nope[4]) +
+                   (nope[0] * nope[1] * nope[2] * nope[3] * yep[4])
+                   )
+    d_probs.append((yep[0] * yep[1] * nope[2] * nope[3] * nope[4]) +
+                   (yep[0] * nope[1] * yep[2] * nope[3] * nope[4]) +
+                   (yep[0] * nope[1] * nope[2] * yep[3] * nope[4]) +
+                   (yep[0] * nope[1] * nope[2] * nope[3] * yep[4]) +
+                   (nope[0] * yep[1] * yep[2] * nope[3] * nope[4]) +
+                   (nope[0] * yep[1] * nope[2] * yep[3] * nope[4]) +
+                   (nope[0] * yep[1] * nope[2] * nope[3] * yep[4]) +
+                   (nope[0] * nope[1] * yep[2] * yep[3] * nope[4]) +
+                   (nope[0] * nope[1] * yep[2] * nope[3] * yep[4]) +
+                   (nope[0] * nope[1] * nope[2] * yep[3] * yep[4])
+                   )
+    d_probs.append((yep[0] * yep[1] * yep[2] * nope[3] * nope[4]) +
+                   (yep[0] * yep[1] * nope[2] * yep[3] * nope[4]) +
+                   (yep[0] * yep[1] * nope[2] * nope[3] * yep[4]) +
+                   (yep[0] * nope[1] * yep[2] * yep[3] * nope[4]) +
+                   (yep[0] * nope[1] * yep[2] * nope[3] * yep[4]) +
+                   (yep[0] * nope[1] * nope[2] * yep[3] * yep[4]) +
+                   (nope[0] * yep[1] * yep[2] * yep[3] * nope[4]) +
+                   (nope[0] * yep[1] * yep[2] * nope[3] * yep[4]) +
+                   (nope[0] * nope[1] * yep[2] * yep[3] * yep[4])
+                   )
+    d_probs.append((yep[0] * yep[1] * yep[2] * yep[3] * nope[4]) +
+                   (yep[0] * yep[1] * yep[2] * nope[3] * yep[4]) +
+                   (yep[0] * yep[1] * nope[2] * yep[3] * yep[4]) +
+                   (yep[0] * nope[1] * yep[2] * yep[3] * yep[4]) +
+                   (nope[0] * yep[1] * yep[2] * yep[3] * yep[4])
+                   )
+    d_probs.append((yep[0] * yep[1] * yep[2] * yep[3] * yep[4]))
+
+    yep = j_yep
+    nope = j_nope
+    j_probs.append(nope[0] * nope[1] * nope[2] * nope[3] * nope[4])
+    j_probs.append((yep[0] * nope[1] * nope[2] * nope[3] * nope[4]) +
+                   (nope[0] * yep[1] * nope[2] * nope[3] * nope[4]) +
+                   (nope[0] * nope[1] * yep[2] * nope[3] * nope[4]) +
+                   (nope[0] * nope[1] * nope[2] * yep[3] * nope[4]) +
+                   (nope[0] * nope[1] * nope[2] * nope[3] * yep[4])
+                   )
+    j_probs.append((yep[0] * yep[1] * nope[2] * nope[3] * nope[4]) +
+                   (yep[0] * nope[1] * yep[2] * nope[3] * nope[4]) +
+                   (yep[0] * nope[1] * nope[2] * yep[3] * nope[4]) +
+                   (yep[0] * nope[1] * nope[2] * nope[3] * yep[4]) +
+                   (nope[0] * yep[1] * yep[2] * nope[3] * nope[4]) +
+                   (nope[0] * yep[1] * nope[2] * yep[3] * nope[4]) +
+                   (nope[0] * yep[1] * nope[2] * nope[3] * yep[4]) +
+                   (nope[0] * nope[1] * yep[2] * yep[3] * nope[4]) +
+                   (nope[0] * nope[1] * yep[2] * nope[3] * yep[4]) +
+                   (nope[0] * nope[1] * nope[2] * yep[3] * yep[4])
+                   )
+    j_probs.append((yep[0] * yep[1] * yep[2] * nope[3] * nope[4]) +
+                   (yep[0] * yep[1] * nope[2] * yep[3] * nope[4]) +
+                   (yep[0] * yep[1] * nope[2] * nope[3] * yep[4]) +
+                   (yep[0] * nope[1] * yep[2] * yep[3] * nope[4]) +
+                   (yep[0] * nope[1] * yep[2] * nope[3] * yep[4]) +
+                   (yep[0] * nope[1] * nope[2] * yep[3] * yep[4]) +
+                   (nope[0] * yep[1] * yep[2] * yep[3] * nope[4]) +
+                   (nope[0] * yep[1] * yep[2] * nope[3] * yep[4]) +
+                   (nope[0] * nope[1] * yep[2] * yep[3] * yep[4])
+                   )
+    j_probs.append((yep[0] * yep[1] * yep[2] * yep[3] * nope[4]) +
+                   (yep[0] * yep[1] * yep[2] * nope[3] * yep[4]) +
+                   (yep[0] * yep[1] * nope[2] * yep[3] * yep[4]) +
+                   (yep[0] * nope[1] * yep[2] * yep[3] * yep[4]) +
+                   (nope[0] * yep[1] * yep[2] * yep[3] * yep[4])
+                   )
+    j_probs.append((yep[0] * yep[1] * yep[2] * yep[3] * yep[4]))
+
+
     for i in range(len(needlist)):
-        prob.append((d_probs[needlist[i][0]] + j_probs[needlist[i][1]]) / 40)
-    print(sum(prob))
+        prob.append((d_probs[needlist[i][0]] * j_probs[needlist[i][1]]))
+
 
     # print(needlist[prob.index(max(prob))])
     b_i = []
@@ -159,12 +255,13 @@ def wisdom(strategies, needs):
         b_i.append(b)
     ge = b_i.index(max(b_i))
 
-    print("ВНИМАНИЕ! ГЕ: ", strategies[ge])
-    print("Вероятный профит:", b_i[ge])
+    print("Выбранная стратегия: ", strategies[ge])
+    # print("Вероятный профит:", b_i[ge])
     print("Но наиболее вероятное развитие событий:", the_str)
-    print("Вероятный профит:", b_i[strategies.index(the_str)])
-    print("Вероятный профит от [3, 3]:", b_i[strategies.index([3, 3])])
-
+    # print("Вероятный профит:", b_i[strategies.index(the_str)])
+    # print("Вероятный профит от [3, 3]:", b_i[strategies.index([3, 3])])
+    # [3, 3] - одна из наиболее живучих стратегий на тестах. Она стабильно вывозит при тестах, так что я вбил
+    # эту строчку для сравнения при отстройке ИИ
     global switcher
     global raz
     global dva
@@ -176,6 +273,7 @@ def wisdom(strategies, needs):
 
 
 def zakup(strtg):
+    """Закупка абстрактного оборудования в соответствии со стратегией"""
     global d_count
     global j_count
     global cash
@@ -183,6 +281,8 @@ def zakup(strtg):
     global j_gl_profits
     global d_sale_price
     global j_sale_price
+    global counters
+
     ge = d_count
     gege = j_count
     sauce = [0, 0]
@@ -193,6 +293,7 @@ def zakup(strtg):
             break
         d_count += 1
         cash -= d_gl_profits[0]
+        counters[4] += 1
         sauce[0] += 1
 
     for i in range(gege, strtg[1]):
@@ -202,11 +303,13 @@ def zakup(strtg):
         j_count += 1
         cash -= j_gl_profits[0]
         sauce[1] += 1
+        counters[5] += 1
     print("Приобретено {} дисплеев и {} джеков, теперь их {} и {}".format(sauce[0], sauce[1], d_count, j_count))
     print("В кассе теперь ", cash, " чего бы то ни было")
 
 
 def repair(customers):
+    """Непосредственно применение стратегии"""
     global cash
     global j_count
     global d_count
@@ -214,29 +317,36 @@ def repair(customers):
     global j_gl_profits
     global d_sale_price
     global j_sale_price
+    global counters
 
     for person in customers:
+        counters[6] += 1
         if person[0] == 1 and d_count > 0:
             d_count -= 1
             cash += d_sale_price
             print("Починен дисплей, получено {} чего бы то ни было".format(d_sale_price))
+            counters[0] += 1
         elif person[0] == 1 and d_count == 0:
             cash += d_gl_profits[2]
             print("Не починен дисплей, репутация ПАДАЕТ!")
+            counters[2] += 1
 
         if person[1] == 1 and j_count > 0:
             j_count -= 1
             cash += j_sale_price
             print("Починен джек, получено {} чего бы то ни было".format(j_sale_price))
+            counters[1] += 1
         elif person[1] == 1 and j_count == 0:
             cash += j_gl_profits[2]
             print("Не починен джек, репутация ПАДАЕТ!")
+            counters[3] += 1
 
     if cash < 0:
         cash = 0
 
 
 def move():
+    """Ход"""
     global cash
     global d_count
     global j_count
@@ -259,7 +369,7 @@ def move():
         j_need = round(random.uniform(0.0, 1.0), 1)
         needs.append([d_need, j_need])
 
-    print("these are NEEDS:", needs)
+    # print("these are NEEDS:", needs)
     chosen = wisdom(strategies, needs)
     # print(chosen)
 
@@ -283,5 +393,20 @@ def move():
 for i in range(i_am_lazy):
     if cash < 40 and j_count == 0 and d_count == 0:
         print("Проиграл за {} дней".format(i))
+        lose = True
         break
     move()
+    time += 1
+if lose:
+    print("Такое нечасто случается, но гениальный искусственный интеллект подвёл наш гипотетический магазинчик.\n \
+За свою недолгую и не очень-то победоносную карьеру он успел обслужить {} клиентов, починив {} дисплеев и \
+{} джеков. Недовольными остались {} клиентов.".format(counters[6], counters[0], counters[1], counters[2] +
+                                                      counters[3]))
+else:
+    print("Игра успешно окончена. За {} ходов разум машины принёс прибыль в {} абстрактных единиц, обслужил {} клиентов,\
+ починил {} дисплеев и {} джеков, но при этом оставил недовольными {} клиентов. \nБыло куплено {} дисплеев и \
+{} джеков. \nВо время работы ни один робот не пострадал.".format(time, cash - 1000, counters[6],
+                                                                 counters[0], counters[1],
+                                                                 counters[2] + counters[3],
+                                                                 counters[4], counters[5]))
+
